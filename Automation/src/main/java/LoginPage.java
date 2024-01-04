9e7aed025ca8dc2309274f4715bf1881662abb87
Volha Raukuts <raukuts.olga@gmail.com>
2024-01-04 20:45:36 +0100

    no message

--- Automation/src/main/java/LoginPage.java
+++ Automation/src/main/java/LoginPage.java
@@ -1,39 +1,30 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
 public class LoginPage {
-    WebDriver driver;
-    public static final String SWAG_LABS = "https://https://www.saucedemo.com/";
+    public WebDriver driver;
 
     public LoginPage(WebDriver driver) {
-        //PageFactory.initElements(driver, this);
         this.driver = driver;
     }
+    By userName = By.xpath("//*[@id=\"user-name\"]");
+    By password = By.xpath("//*[@id=\"password\"]");
+    By loginButton = By.xpath("//*[@id=\"login-button\"]");
 
-    @FindBy(id = "user-name")
-    public WebElement usernameField;
-
-    @FindBy(id = "password")
-    public WebElement passwordField;
-
-    @FindBy(id = "login-button")
-    public WebElement loginButton;
-
-    public void setUsernameField(WebElement usernameField) {
-        this.usernameField = usernameField;
-        usernameField.sendKeys("standard_user");
+    public void setUsernameField(String username) {
+        driver.findElement(userName).sendKeys(username);
         System.out.println("The username was entered");
     }
 
-    public void setPasswordField(WebElement passwordField) {
-        this.passwordField = passwordField;
-        passwordField.sendKeys("secret_sauce");
+    public void setPasswordField(String passwordU) {
+        driver.findElement(password).sendKeys(passwordU);
         System.out.println("The password was entered");
     }
 
-    public void clickLoginButton(WebElement loginButton){
-        loginButton.click();
+    public void clickLoginButton(){
+        driver.findElement(loginButton).click();
+    }
+    public void login(String strUserName, String strPasword) {
+        this.setUsernameField(strUserName);
+        this.setPasswordField(strPasword);
     }
 }
--- Automation/src/test/java/LoginTests.java
+++ Automation/src/test/java/LoginTests.java
@@ -1,21 +1,35 @@
+import org.example.ConfProperties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class LoginTests {
-    public  WebDriver driver = new FirefoxDriver();
+    public static LoginPage loginPage;
+    @BeforeTest
+    public static void setup(){
+        System.setProperty("webdriver.firefox.driver", ConfProperties.getProperty("firefoxdriver"));
+        WebDriver driver = new FirefoxDriver();
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.get(ConfProperties.getProperty("loginpage"));
+    }
 
     @Test(description = "open Log in page and find some elements")
     public void logInPage(){
+        loginPage.login("standard_user", "secret_sauce");
+        loginPage.clickLoginButton();
+       // Assert.assertEquals();
+    }
 
-        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-        //loginPage.waitConditions(loginPage.usernameField, Duration.ofSeconds(20));
-        loginPage.setUsernameField(loginPage.usernameField);
-        loginPage.setPasswordField(loginPage.passwordField);
-        Assert.assertEquals(loginPage.loginButton, loginPage.loginButton);
-        //loginPage.openHomePage();
+    @Test(description = "enter wrong credentials")
+    public void logInPageWrong(){
+        loginPage.login("standart_user", "secrets_sauce");
+        loginPage.clickLoginButton();
+        Assert.assertEquals("password do not match any user in this service", "password do not match any user in this service");
     }
 
 }
