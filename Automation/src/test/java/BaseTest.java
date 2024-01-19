8e9add2f92dfdd416919402e0d97563348cf18f6
Volha Raukuts <raukuts.olga@gmail.com>
2024-01-19 20:17:57 +0100

    just refactored some code and tests

--- Automation/src/test/java/BaseTest.java
+++ Automation/src/test/java/BaseTest.java
@@ -1,7 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.ConfProperties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
     public static  WebDriver driver;
@@ -9,12 +14,14 @@ public class BaseTest {
     public static LoginPage loginPage;
     public static ProductPage productPage;
     public static CartPage cartPage;
+    static Logger log = LogManager.getLogger();
     @BeforeClass
     public static void setup(){
         System.setProperty("webdriver.firefox.driver", ConfProperties.getProperty("firefoxdriver"));
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(ConfProperties.getProperty("loginpage"));
+        log.info("Setup finished");
     }
     @BeforeMethod
     public static void setupPreconditions(){
--- Automation/src/test/java/CartPageTests.java
+++ Automation/src/test/java/CartPageTests.java
@@ -2,7 +2,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CartPageTests extends BaseTest{
-    @Test(description = "add a product to the cart and finish the buying")
+    @Test(description = "add a product to the cart and finish the buying", priority = 1)
     public void orderProduct(){
         BaseTest.setupPreconditionsCart();
         cartPage.clickCheckoutButton();
@@ -13,34 +13,33 @@ public class CartPageTests extends BaseTest{
         cartPage.clickFinishButton();
         Assert.assertTrue(cartPage.getFinishedOrder());
     }
-   @Test(description = "remove a product from the cart")
+   @Test(description = "remove a product from the cart", priority = 2)
     public void removeProduct(){
         BaseTest.setupPreconditionsCart();
         cartPage.removeProduct();
        // Assert.assertTrue(cartPage.getCartItem()); подумать над ассертом
         cartPage.clickContinueShopping();
     }
-     @Test(description = "checkout without products")
-    public void checkoutEmptyCart(){
-        BaseTest.setupPreconditionsCart();
-        cartPage.clickCheckoutButton();
-        cartPage.fillTheForm("olga","ra","s233");
-        cartPage.clickContinueButton();
-        cartPage.clickFinishButton();
-        Assert.assertTrue(cartPage.getFinishedOrder());
+     @Test(description = "checkout without products", priority = 3)
+    public void checkoutEmptyCart() {
+         BaseTest.setupPreconditionsCart();
+         log.info("Sample INFO message");
+         cartPage.clickCheckoutButton();
+         cartPage.fillTheForm("olga", "ra", "s233");
+         cartPage.clickContinueButton();
+         cartPage.clickFinishButton();
+         Assert.assertTrue(cartPage.getFinishedOrder());
+     }
 
-    }
- /* @Test(description = "checkout without filling checkout form")
+    @Test(description = "checkout without filling checkout form", priority = 4)
     public void checkoutEmptyCheckoutForm(){
         BaseTest.setupPreconditionsCart();
         cartPage.clickCheckoutButton();
         cartPage.clickContinueButton();
         Assert.assertTrue(cartPage.getErrorFN().contains("Error: First Name is required"));
         cartPage.clickCancelButton();
-        cartPage.clickContinueShopping();
+        //cartPage.clickContinueShopping();
         //productsPage.clickSideBarMenu();
     }
-
-  */
 
 }
