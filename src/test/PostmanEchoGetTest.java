129f32ed8aaf2c8ce386b11a21f8b73bf7bce3ec
Volha Raukuts <raukuts.olga@gmail.com>
2024-02-19 09:12:11 +0100

    some alterations were made

--- src/test/PostmanEchoGetTest.java
+++ src/test/PostmanEchoGetTest.java
@@ -1,19 +1,7 @@
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.given;
 
 public class PostmanEchoGetTest {
-
-@Test
-    public void useSpec(){
-    given()
-            .spec(Configs.spec)
-            .param("limit",20)
-            .when()
-            .get("blogs")
-            .then()
-            .statusCode(200);
-}
 
     @Test
     public void postmanFirstGetTest(){
