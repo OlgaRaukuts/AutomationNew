b18e1e8f72237d15703d500498a8d543f58cd4ec
Volha Raukuts <raukuts.olga@gmail.com>
2024-02-19 21:51:38 +0100

    added some tests

---
+++ src/test/PostmanEchoDeleteTests.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class PostmanEchoDeleteTests {
+    @Test
+    public void deletePositive(){
+        
+    }
+}
--- src/test/PostmanEchoGetTest.java
+++ src/test/PostmanEchoGetTest.java
@@ -4,10 +4,16 @@ import org.testng.annotations.Test;
 public class PostmanEchoGetTest {
 
     @Test
-    public void postmanFirstGetTest(){
+    public void firstGetTest(){
         RestAssured.
                 when().get("https://postman-echo.com/get").
                 then().assertThat().statusCode(200);
+    }
+    @Test
+    public void getTestNegative(){
+        RestAssured.
+                when().get("https://postman-echo.com/get/olya").
+                then().assertThat().statusCode(404);
     }
 }
 
