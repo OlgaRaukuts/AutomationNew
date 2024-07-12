b3ec8eb19b1957503743586326ca7ff4a0255b10
Volha Raukuts <raukuts.olga@gmail.com>
2024-07-12 11:20:41 +0200

    no message

--- JavaTraining/src/Main.java
+++ JavaTraining/src/Main.java
@@ -1,21 +1,11 @@
-import Challenge1to10.MethodChallenge;
-import Challenge1to10.PositiveNegativeZero;
+import Challenge1to10.MegaBytesConverter;
+import Challenge1to10.SpeedConverter;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        int highScorePosition = MethodChallenge.calculateHighScorePosition(1500);
-        MethodChallenge.displayHighScorePosition("Tim", highScorePosition);
-
-        highScorePosition = MethodChallenge.calculateHighScorePosition(1000);
-        MethodChallenge.displayHighScorePosition("Jane", highScorePosition);
-        highScorePosition = MethodChallenge.calculateHighScorePosition(500);
-        MethodChallenge.displayHighScorePosition("James", highScorePosition);
-        highScorePosition = MethodChallenge.calculateHighScorePosition(100);
-        MethodChallenge.displayHighScorePosition("Teddy", highScorePosition);
-        highScorePosition = MethodChallenge.calculateHighScorePosition(25);
-        MethodChallenge.displayHighScorePosition("Wayne", highScorePosition);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
     }
 }
