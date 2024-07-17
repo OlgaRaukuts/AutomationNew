0d0ec051423ee518c51120ce2a6c655173e5b500
Volha Raukuts <raukuts.olga@gmail.com>
2024-07-17 21:02:00 +0200

    no message

---
+++ JavaTraining/src/Challenge1to10/MethodsOverloading.java
@@ -0,0 +1,13 @@
+package Challenge1to10;
+
+public class MethodsOverloading {
+    public static void convertToCentimeters (int heightInches){
+        double centimeters = heightInches * 2.54;
+        System.out.println(centimeters);
+
+    }
+    public static void convertToCentimeters (int heightFeet, int heightInches ){
+        double feet = ((heightFeet *12) + heightInches)*2.54;
+        System.out.println(feet);
+    }
+}
--- JavaTraining/src/Main.java
+++ JavaTraining/src/Main.java
@@ -5,6 +5,6 @@ import Challenge1to10.*;
 public class Main {
     public static void main(String[] args) {
 
-        TeenNumberChecker.hasTeen(22, 23, 34);
+        System.out.println(PlayingCat.isCatPlaying(true,45));
     }
 }
