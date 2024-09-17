eaabdf68bf4a92ddd9ab68adf6e16d9d889546ff
Volha Raukuts <raukuts.olga@gmail.com>
2024-09-17 10:11:29 +0200

    no message

--- JavaTraining/src/main/java/Main.java
+++ JavaTraining/src/main/java/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        int number = 4;
+        int finishNumber = 24;
 
+        while (number <= finishNumber){
+            number++;
+            if(!isEvenNumber(number)){
+                continue;
+            }
+            System.out.println("Even number is " + number);
+        }
+
+
+    }
+    public static boolean isEvenNumber(int number){
+
+        if((number % 2) == 0){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
